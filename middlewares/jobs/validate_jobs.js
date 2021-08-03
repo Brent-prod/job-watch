c74cb836c5c7f203edf33dd78cae721601f6ed77
Adan Moses <adanmoses@pm.me>
2021-08-03 19:18:49 +1000

    added job validator

---
+++ middlewares/jobs/validate_jobs.js
@@ -0,0 +1,30 @@
+function validationError(message) {
+  const error = new Error(message);
+  error.status = 422;
+  return error;
+}
+
+function validateJob(req, res, next) {
+  const role = req.body.role
+  const company = req.body.company
+  const close_date = req.body.close_date
+  const contact = req.body.contact
+
+  // TODO should probably be "isBlank(role)" - write a method
+  if (role === '' || role === undefined || role === null) {
+    throw validationError("Role is required");
+  }
+  else if (company === '' || company === undefined || company === null) {
+    throw validationError("Company is required");
+  }
+  else if (close_date === '' || close_date === undefined || close_date === null) {
+    throw validationError("Close Date is required");
+  }
+  else if (contact === '' || contact === undefined || contact === null) {
+    throw validationError("Contact is required");
+  }
+
+  next();
+}
+
+module.exports = validateJob;
