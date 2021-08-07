f6a36b8ed237bfd31a66f835e1b4093d8d7ad8e1
Adan Moses <adanmoses@pm.me>
2021-08-07 16:44:51 +1000

    fixed session secret for heroku, for reals this time, but for reals this time, but like, for reals this time

--- db/db.js
+++ db/db.js
@@ -1,8 +1,18 @@
-const pg = require('pg')
+const { Pool } = require('pg')
 
-const db = new pg.Pool({
-  database: 'jobwatch',
-  password: process.env.DB_PASSWORD
-})
+let db;
+if (process.env.NODE_ENV === 'production') {
+  db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+   db = new Pool({
+    database: 'jobwatch',
+    password: process.env.DB_PASSWORD
+  })
+}
 
 module.exports = db
