3738eef7d2fae5ddc3f51a6882e5c3aab0ee3e32
lesliec <leslie.cayas@gmail.com>
2021-08-08 15:39:57 +1000

    css for buttons

--- client/js/login.js
+++ client/js/login.js
@@ -1,27 +1,37 @@
 function getUser() {
+
   axios.get('/api/sessions')
-  .then(sessionInfo => {
-    if (sessionInfo.data.userName){
-      // if logged in
-      state.user = sessionInfo.data.userName;
+    .then(sessionInfo => {
+      if (sessionInfo.data.userName) {
+        // if logged in
+        state.user = sessionInfo.data.userName;
         document.querySelector('#userName')
-        .innerHTML = `
-        <p> Welcome, ${sessionInfo.data.userName}!</p>
+          .innerHTML = `
+        <p> Welcome, ${sessionInfo.data.userName}! </p>
         <button id="logout">Logout</button>
         `
 
         // to make a delete request to log out a user
-        document.querySelector('#logout').addEventListener('click', event => {
+        document.querySelector('#logout').addEventListener('click', () => {
           axios.delete('/api/sessions').then(() => {
             window.location = '/index.html'
           })
         })
-        // if not logged in
-    } else {
-        document.querySelector('#userName')
-        .innerHTML = sessionInfo.data.error
-    }
-  })
+      }
+      // else {
+      //   // if not logged in
+      //   console.log("hi")
+      //   document.querySelector('#userName')
+      //     .innerHTML = sessionInfo.data.error
+      // }
+    })
+    .catch(error => {
+      // console.log(error)
+      document.getElementById("errors").style.display = "block"
+
+      document.querySelector('#errors')
+        .innerHTML = error.response.data.error;
+    })
 }
 
 getUser()
