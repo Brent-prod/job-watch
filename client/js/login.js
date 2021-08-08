154824c2ab064e113cdea47281b902d27c38fa0f
Adan Moses <adanmoses@pm.me>
2021-08-08 15:41:22 +1000

    started doing some css

--- client/js/login.js
+++ client/js/login.js
@@ -1,25 +1,25 @@
 function getUser() {
-  axios.get('/api/sessions')
-  .then(sessionInfo => {
-    if (sessionInfo.data.userName){
+  axios.get("/api/sessions").then((sessionInfo) => {
+    if (sessionInfo.data.userName) {
       // if logged in
       state.user = sessionInfo.data.userName;
-        document.querySelector('#userName')
-        .innerHTML = sessionInfo.data.userName + ' is logged in ' + '<br><button id="logout">Logout</button>'
-        document.getElementById("login").style.display = "none";
+      document.querySelector("#userName").innerHTML =
+        "Welcome " +
+        sessionInfo.data.userName +
+        '<br><button id="logout">Logout</button>';
+      document.querySelector("#login").style.display = "none";
 
-        // to make a delete request to log out a user
-        document.querySelector('#logout').addEventListener('click', event => {
-          axios.delete('/api/sessions').then(() => {
-            window.location = '/index.html'
-          })
-        })
-        // if not logged in
+      // to make a delete request to log out a user=
+      document.querySelector("#logout").addEventListener("click", (event) => {
+        axios.delete("/api/sessions").then(() => {
+          window.location = "/index.html";
+        });
+      });
+      // if not logged in
     } else {
-        document.querySelector('#userName')
-        .innerHTML = sessionInfo.data.error
+      document.querySelector("#userName").innerHTML = sessionInfo.data.error;
     }
-  })
+  });
 }
 
-getUser()
+getUser();
