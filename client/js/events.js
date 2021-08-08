5932f1625181fceeea7b90734fc7ff8dcbe68eae
lesliec <leslie.cayas@gmail.com>
2021-08-08 22:47:12 +1000

    highlights job posting when close date is less than a week away

--- client/js/events.js
+++ client/js/events.js
@@ -25,7 +25,25 @@ function events() {
 
 function compareDates() {
   const currentDate = new Date()
-  const jobDates = state.jobs.map(job => new Date(job.close_date).toISOString().slice(0,10))
-  
-  return jobDates
+  const jobDates = state.jobs.map(job => new Date((job.close_date).slice(0,10)))
+
+  for (let i = 0; i < jobDates.length; i++) {
+    if (dateDiffInDays(currentDate, jobDates[i]) <= 7) {
+      document.querySelector(`[data-id='${state.jobs[i].id}']`)
+        .style.border = "3px solid red"
+    }
+  }
+}
+
+
+// a and b are javascript Date objects
+function dateDiffInDays(currentDate, closeDate) {
+  // Discard the time and time-zone information.
+  const msPerDay = 1000 * 60 * 60 * 24;
+
+  const utc1 = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  const utc2 = Date.UTC(closeDate.getFullYear(), closeDate.getMonth(), closeDate.getDate());
+
+
+  return Math.floor((utc2 - utc1) / msPerDay);
 }
